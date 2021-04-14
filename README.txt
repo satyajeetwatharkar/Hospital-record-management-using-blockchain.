cbdf49f07d44685a497cb1a2ef254fafe943be0f
Satyajeet <satyajeetwatharkar@gmail.com>
2021-04-14 18:36:31 +0530

    Second Commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+Meme-   doctor.html
+	doctor_details.html
+	staffMember.html
+	staffMemberDetauls.html
+	PatientRegistration.html
+	patient_details.html
+Insurance -	Medical record
+		record_details
+Scan-		examine_details
+		patient_examine
--- app.js
+++ app.js
@@ -1,11 +1,127 @@
 //Particula
-const express=require('express')
-const bodyParser=require('body-parser');
-const app=express();
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose =
+        require("passport-local-mongoose"),
+    User = require("./models/user");
 
 
-app.use(bodyParser.urlencoded({extended:true}))
+    mongoose.set('useNewUrlParser', true);
+    mongoose.set('useFindAndModify', false);
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useUnifiedTopology', true);
+    mongoose.connect("mongodb://localhost/hospital");
+
+
+
+
+
+    var app = express();
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+
+    app.use(require("express-session")({
+        secret: "Rusty is a dog",
+        resave: false,
+        saveUninitialized: false
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(new LocalStrategy(User.authenticate()));
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+
 app.use(express.static("public"));
+//Copied
+
+// Showing secret page
+app.get("/secret", isLoggedIn, function (req, res) {
+    res.sendFile(__dirname+"/Frontpage.html");
+});
+
+// Showing register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+// Handling user signup
+app.post("/register", function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    User.register(new User({ username: username }),
+            password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+
+        passport.authenticate("local")(
+            req, res, function () {
+              res.sendFile(__dirname+"/Frontpage.html");
+        });
+    });
+});
+
+//Showing login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+//Handling user login
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/"
+}), function (req, res) {
+});
+
+//Handling user logout
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -14,11 +130,12 @@ app.get("/",function(req,res){
   res.sendFile(__dirname+"/Front.html");
 
 });
-app.get("/Frontpage",function(req,res){
+/* app.get("/Frontpage",function(req,res){
 
   res.sendFile(__dirname+"/Frontpage.html");
 
 });
+*/
 
 app.get("/staffMember",function(req,res){
 
@@ -121,6 +238,12 @@ res.send("Thanks for posting");
 });
 
 
-app.listen(3000,function(){
+/*app.listen(3000,function(){
   console.log("Server started");
+});
+*/
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Has Started!");
 });
